68d338f6318da888d293297491e56d6e3949aeab
RinseYaron <92859617+norayesnir@users.noreply.github.com>
2023-06-15 12:50:45 +0200

    hello, world!

--- www/app.vue
+++ www/app.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Hello, World!</h1>
+    <p>Welcome to the Hasura + Vercel + Nuxt project where i will be making a clone of Twitter. At the end of this project, you should be able to post your tweets, make your account, and see tweets from other users.</p>
   </div>
 </template>
+
+<style scoped lang="scss">
+div{
+  width: fit-content;
+  height: fit-content;
+  margin: 100px auto;
+
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+
+  h1{
+    text-transform: uppercase;
+  }
+
+  p{
+    max-width: 400px;
+    line-height: 1.7;
+  }
+}
+</style>
